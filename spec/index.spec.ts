8b4f3c86066fd3b8a77f187894f89fd4d28c982f
Yuanchieh <sj82516@gmail.com>
2021-06-13 07:35:31 +0800

    feat: do function

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,8 +1,40 @@
 import 'jest'
 import SingleFlight from "../src";
 
-describe('SingleFlight', ()=>{
-    it('should be init', function () {
-        let singleFlight = new SingleFlight()
+describe('SingleFlight', () => {
+    let singleFlight: SingleFlight
+    beforeEach(() => {
+        singleFlight = new SingleFlight()
+    })
+    it('should only call source once under SingleFlight.do protection', async function () {
+        const result = "hello singleflight"
+        let sourceFn = jest.fn(() => {
+            return new Promise((res, rej) => {
+                setTimeout(() => res(result), 1000)
+            })
+        })
+        let protectFn = singleFlight.do('key', sourceFn)
+        let returnValue = null;
+        for (let i = 0; i < 10; i++) {
+            returnValue = await protectFn()
+        }
+        expect(sourceFn.mock.calls.length).toBe(1)
+        expect(returnValue).toBe(result)
+    });
+    it('make sure the params are pass in', async function () {
+        const params = 'test'
+        const expectResult = 'test, singleflight'
+        let sourceFn = jest.fn((input) => {
+            return new Promise((res, rej) => {
+                setTimeout(() => res(`${input}, singleflight`), 1000)
+            })
+        })
+        let protectFn = singleFlight.do('key', sourceFn)
+        let returnValue = null;
+        for (let i = 0; i < 10; i++) {
+            returnValue = await protectFn(params)
+        }
+        expect(sourceFn.mock.calls.length).toBe(1)
+        expect(returnValue).toBe(expectResult)
     });
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 export default class SingleFlight {
+    private map: Map<string, Function>;
+
     constructor() {
+        this.map = new Map()
+    }
+
+    do(key: string, sourceFn: Function): (...args: any[]) => Promise<any | Function | undefined> {
+        return async (...args) => {
+            const runningFn = this.map.get(key)
+            if (runningFn) {
+                return runningFn;
+            }
+            this.map.set(key, await sourceFn(...args))
+            return this.map.get(key)
+        }
     }
 }
