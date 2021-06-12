1f9d8a62badb19bd1958340b3ed71bbf7aff2bc5
Yuanchieh <sj82516@gmail.com>
2021-06-13 07:04:40 +0800

    feat: init

---
+++ spec/index.spec.ts
@@ -0,0 +1,8 @@
+import 'jest'
+import SingleFlight from "../src";
+
+describe('SingleFlight', ()=>{
+    it('should be init', function () {
+        let singleFlight = new SingleFlight()
+    });
+})
